d4387ab4bf0219e00fa5fc69818541e8bd74bea1
tonik <ru.antondrozdov@gmail.com>
2019-06-22 23:20:34 -0700

    Merge

--- src/pages/bcdvazaar/shop-target/shop-target.ts
+++ src/pages/bcdvazaar/shop-target/shop-target.ts
@@ -1,4 +1,4 @@
-import { Component  } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 import { Logger } from '../../../providers';
@@ -14,27 +14,27 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-shop-target',
-  templateUrl: 'shop-target.html',
+  templateUrl: 'shop-target.html'
 })
 export class ShopTargetPage {
-private shop: SafeResourceUrl;
-private url: string = '';
-private logo: string = '';
-  constructor(public navCtrl: NavController, public navParams: NavParams, public logger: Logger, public sanitizer:DomSanitizer) {
-    
+  public shop: SafeResourceUrl;
+  private url: string = '';
+  private logo: string = '';
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public logger: Logger,
+    public sanitizer: DomSanitizer
+  ) {
     this.url = navParams.get('shop');
     this.logo = navParams.get('logo');
     this.shop;
     this.logo;
-
   }
 
-  ionViewDidLoad() {
-    
-  }
+  ionViewDidLoad() {}
 
   ngOnInit() {
-    this.shop = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);      
-}
-
+    this.shop = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
 }
